8e604b796921c4c2cd972271d2ff9b7d424a448f
D. Leonard fredrick <124865112+Fredrick2216@users.noreply.github.com>
2025-03-19 11:19:15 +0530

    Add files via upload

---
+++ FrontEnd /src/vite-env.d.ts
@@ -0,0 +1,33 @@
+
+/// <reference types="vite/client" />
+
+interface Window {
+  SpeechRecognition: typeof SpeechRecognition;
+  webkitSpeechRecognition: typeof SpeechRecognition;
+}
+
+interface SpeechRecognitionErrorEvent extends Event {
+  error: string;
+  message: string;
+}
+
+interface SpeechRecognitionEvent extends Event {
+  resultIndex: number;
+  results: SpeechRecognitionResultList;
+}
+
+interface SpeechRecognition extends EventTarget {
+  continuous: boolean;
+  interimResults: boolean;
+  lang: string;
+  onaudiostart: (event: Event) => void;
+  onaudioend: (event: Event) => void;
+  onend: (event: Event) => void;
+  onerror: (event: SpeechRecognitionErrorEvent) => void;
+  onnomatch: (event: SpeechRecognitionEvent) => void;
+  onresult: (event: SpeechRecognitionEvent) => void;
+  onstart: (event: Event) => void;
+  start: () => void;
+  stop: () => void;
+  abort: () => void;
+}
